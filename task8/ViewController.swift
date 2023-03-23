0bf380f4f4cd7bf5a649badeb20f632bf8cf532d
Gleb Tregubov <tregubovgleb@gmail.com>
2023-03-23 07:21:56 +0300

    done

--- task8/ViewController.swift
+++ task8/ViewController.swift
@@ -11,9 +11,38 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        title = "Avatar"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    
+        let scrollView = UIScrollView(frame: view.bounds)
+        scrollView.contentSize = CGSize(width: view.bounds.width, height: 1000)
+        view.addSubview(scrollView)
+        navigationController?.navigationBar.setLargeTitleRightView()
+        
     }
-
-
 }
 
+extension UINavigationBar {
+    func setLargeTitleRightView() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            guard let UINavigationBarLargeTitleView = NSClassFromString("_UINavigationBarLargeTitleView") else { return }
+            guard let largeTitleView = self.subviews.first(where: {
+                $0.isKind(of: UINavigationBarLargeTitleView.self)
+            }) else { return }
+            
+            let imageView = UIImageView(image: UIImage(systemName: "person.crop.circle.fill"))
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            imageView.tintColor = .lightGray
+            
+            largeTitleView.addSubview(imageView)
+            NSLayoutConstraint.activate([
+                imageView.widthAnchor.constraint(equalToConstant: 36),
+                imageView.heightAnchor.constraint(equalToConstant: 36),
+                imageView.trailingAnchor.constraint(equalTo: largeTitleView.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+                imageView.bottomAnchor.constraint(equalTo: largeTitleView.bottomAnchor, constant: -6)
+            ])
+        }
+    }
+}
